8ec8b4252510bb130239e14aaf580766463c14d2
Fahim Kabir <fkabir101@gmail.com>
2019-02-08 13:38:33 -0500

    Initial commit/project setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(routes);
+
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
