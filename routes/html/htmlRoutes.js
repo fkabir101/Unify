37f368be86f966de85a2e7c818dd18a9f2d17d93
cjrojas72 <chris@Christians-MacBook-Pro.local>
2019-02-11 13:13:04 -0500

    saving work

--- routes/html/htmlRoutes.js
+++ routes/html/htmlRoutes.js
@@ -5,8 +5,8 @@ router.route("/").get(function(req, res) {
   res.sendFile(path.join(__dirname, "../../public/login.html"));
 });
 
-router.route("/login").get(function(req, res) {
-  res.sendFile(path.join(__dirname, "../../public/index.html"));
+router.route("/main").get(function(req, res) {
+  res.sendFile(path.join(__dirname, "../../public/mainpage.html"));
 });
 router.route("/create").get(function(req, res) {
   res.sendFile(path.join(__dirname, "../../public/create.html"));
